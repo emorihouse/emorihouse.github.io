53048bba45dda0eadaef5f31f1c523052cf83ab4
cobachie <tanacobachie@gmail.com>
2018-06-27 20:30:48 +0900

    Update GuestHouse Data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,44 @@
 var map;
-var marker;
-var info_baloon;
-var center = {
-  lat: 33.6044276, // 緯度,
-  lng: 130.4034843 // 経度
-};
+var markers = [];
+var infoBalloons = [];
+var markerData = [
+  {
+    name: 'ハーバーサイド ゲストハウス 湊',
+    lat: 33.603186,
+    lng: 130.406397
+  }, {
+    name: "国際コンベンションセンター",
+    lat: 33.6044276,
+    lng: 130.4034843
+  }
+];
+
 function initMap() {
+  // 地図の作成
+  var mapLatLng = new google.maps.LatLng({lat: markerData[0]['lat'], lng: markerData[0]['lng']});
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: center,
-    zoom: 16 // 地図のズームを指定
+    center: mapLatLng,
+    zoom: 16
   });
 
-  marker = new google.maps.Marker({
-    position: center,
-    map: map
-  });
+  // マーカー毎の処理
+  for (var i = 0; i < markerData.length; i++) {
+    markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']});
+    markers[i] = new google.maps.Marker({
+      position: markerLatLng,
+      map: map
+    });
 
-  info_baloon = new google.maps.InfoWindow({
-    content: '<div class="map-canvas">RubyKaigi 2019 会場</div>'
-  });
+    infoBalloons[i] = new google.maps.InfoWindow({ // 吹き出しの追加
+      content: '<div class="map-canvas">' + markerData[i]['name'] + '</div>'
+    });
 
-  marker.addListener('click', function() {
-    info_baloon.open(map, marker);
-  })
+    markerEvent(i);
+  }
+}
+
+function markerEvent(i) {
+  markers[i].addListener('click', function() {
+    infoBalloons[i].open(map, markers[i]);
+  });
 }
